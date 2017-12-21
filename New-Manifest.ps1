e9e8554ea9ccb886f8d51780d2ca54af08ba8eb7
Roy Atkins <atkinsroy@hotmail.com>
2017-12-21 15:04:45 +1100

    Added Get-FileListing function

---
+++ New-Manifest.ps1
@@ -0,0 +1,13 @@
+﻿# 
+# Create a new Manifest for FileAnalysis
+#
+
+New-ModuleManifest -Path '.\FileAnalysis.psd1'  `
+                   -Author 'Roy Atkins'                                                   `
+                   -CompanyName 'Hewlett Packard Enterprise'                              `
+                   -ModuleVersion 0.1.0                                                   `
+                   -RootModule '.\FileAnalysis.psm1'                                      `
+                   -FunctionsToExport Get-FileListing, Get-FileAnalysis                   `
+                   -Description 'Active Directory user maintenance module for McDonalds'  `
+                   -Copyright '(c) 2017 Hewlett Packard Enterprise. All rights reserved.' `
+                   -PowerShellVersion 3.0
